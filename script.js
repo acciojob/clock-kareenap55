6fc90f5b491e12f5d4819ddc549b1222e0b131a1
kareenap55 <kareenap1412@gmail.com>
2025-04-03 11:23:11 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function timeUpdate(){
+ 	let timer = document.getElementById("timer");
+ 	let currentDate = new Date();
+ 	timer.textContent = currentDate.toLocaleString();
+ }
+ timeUpdate();
+ setInterval(timeUpdate, 1000);
